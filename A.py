820b757e7cb1ba6f8eab1fd2b172648aa027a265
jtgoood <150081120+jtgoood@users.noreply.github.com>
2023-11-13 16:20:52 -0500

    Add files via upload

--- A.py
+++ A.py
@@ -2,12 +2,12 @@
 # you aren't going to know what they are doing until after they push their changes
 # if you need a refresher of what a madlib is, go here: https://www.madlibs.com/MadLibs-Teachers-Guide-nocrops.pdf
 
-year_in_the_future = 0 # assign numbers or strings to the end of these versions
-wacky_robot_name = ""
-adjective_1 = ""
-violent_verb = ""
-tech_company = ""
-dollar_amount = ""
-adjective_2 = ""
-noun_1 = ""
-large_store = ""
+year_in_the_future = 1000000 # assign numbers or strings to the end of these versions
+wacky_robot_name = "Samwell"
+adjective_1 = "kind"
+violent_verb = "assault"
+tech_company = "Boston Dynamics"
+dollar_amount = "$4,000"
+adjective_2 = "stupid"
+noun_1 = "doggy"
+large_store = "Target"
